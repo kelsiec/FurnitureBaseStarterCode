2fb6275ebbcf4cec154bbbf776df97c2fdf421d6
kelsiec <kelsiec@maizeanalytics.com>
2019-09-01 14:41:50 -0700

    starter code for Furniture 1

---
+++ src/main/java/Destruction.java
@@ -0,0 +1,7 @@
+public class Destruction {
+    public static void main(String[] args) {
+        // Create two instances of Furniture, a Plywood Chair and a Glass table
+        // Apply each kind of damage to each instance of your furniture,
+        // printing its status at each step
+    }
+}
---
+++ src/main/java/Furniture.java
@@ -0,0 +1,12 @@
+public class Furniture {
+    private String material;
+    private String furnitureType;
+    private boolean isBroken;
+    private boolean isBurnt;
+
+    public Furniture(String furnitureType, String materialType) {
+        // You write this part
+    }
+
+    // You write any functions needed to change state and print the status of the furniture instance
+}
